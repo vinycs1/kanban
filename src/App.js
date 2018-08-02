24c4e5bd0930704745b079c3d2d78a4727557850
vinycs <vinycastro1@gmail.com>
2018-08-02 03:04:48 -0300

    remoção de codigo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, { Component } from 'react';
-import HTML5Backend from 'react-dnd-html5-backend'
-import { DragDropContext } from 'react-dnd'
-import { Row } from 'reactstrap'
-import tickets from './mocks/Tickets.mock'
+import React, { Component } from 'react'
 import './App.css';
 
-import TicketList from './components/TicketList/'
-import CreateTicket from './components/CreateTicket/'
+import Kanban from './views/Kanban/Kanban'
 
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tickets
-    }
-
-    this.ticketStatus = {
-      "todo": "todo",
-      "doing": "doing",
-      "done": "done"
-    }
-
-    this.saveTicket = this.saveTicket.bind(this)
-    this.moveTicket = this.moveTicket.bind(this)
-    this.updatedTicketTime = this.updatedTicketTime.bind(this)
-    this.addTicketTag = this.addTicketTag.bind(this)
-    this.removeTicketTag = this.removeTicketTag.bind(this)
-  }
-
   render() {
-    console.log("render", tickets)
     return (
       <div className="App">
         <header className="App-header">
@@ -39,98 +14,12 @@ class App extends Component {
           </h1>
           <h6>by Vinicius Castro</h6>
         </header>
-        <Row>
-          <CreateTicket saveTicket={this.saveTicket} />
-        </Row>
-        <Row>
-          {this.renderTicketLists()}
-        </Row>
+        <Kanban />
       </div>
-
     )
   }
 
-  renderTicketLists() {
-    return (
-      Object.values(this.ticketStatus).map(status => {
-        return <TicketList
-          key={status}
-          status={status}
-          tickets={this.filterTicketsByStatus(status)}
-          moveTicket={this.moveTicket}
-          updatedTicketTime={this.updatedTicketTime}
-          addTicketTag={this.addTicketTag}
-          removeTicketTag={this.removeTicketTag}
-        />
-      })
-    )
-  }
 
-  filterTicketsByStatus(status) {
-    return Object.values(this.state.tickets).filter(ticket => {
-      return ticket.status === status
-    })
-  }
-
-  saveTicket(ticket) {
-    if (this.verifyIfTicketAlreadyExist(ticket)) {
-      this.setState(prevState => {
-        let tickets = prevState.tickets
-        tickets[ticket.id] = ticket
-        return { tickets }
-      })
-    }
-  }
-
-  verifyIfTicketAlreadyExist(newTicket) {
-    const tickets = Object.values(this.state.tickets)
-    return !tickets.some(ticket => {
-      return ticket.id === newTicket.id || ticket.name === newTicket.name
-    })
-  }
-
-  moveTicket(movingTicket, newStatus) {
-    this.setState(prevState => {
-      let tickets = prevState.tickets
-
-      tickets[movingTicket.id] = this.updateTicketStatus(movingTicket, newStatus)
-
-      return { tickets }
-    })
-  }
-
-  updateTicketStatus(ticket, status) {
-    let updatedTicket = Object.assign({}, ticket)
-    updatedTicket.status = status
-    return updatedTicket
-  }
-
-  updatedTicketTime(ticket, time) {
-    this.setState(prevState => {
-      let tickets = prevState.tickets
-      tickets[ticket.id]["time"] = time
-
-      return { tickets }
-    })
-  }
-
-  addTicketTag(ticket, tag) {
-    this.setState(prevState => {
-      let tickets = prevState.tickets
-      tickets[ticket.id]["tags"][tag.id] = ticket
-
-      return { tickets }
-    })
-  }
-
-  removeTicketTag(ticket, tag) {
-    this.setState(prevState => {
-      let tickets = prevState.tickets
-      delete tickets[ticket.id]["tags"][tag.id]
-
-      return { tickets }
-    })
-  }
 }
 
-export default DragDropContext(HTML5Backend)(App)
+export default App
